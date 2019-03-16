a40d56d17b7017347ac547fe98250652332d26f7
Owen <owen2862@live.com>
2019-03-16 16:56:20 +0800

    order

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -3,38 +3,30 @@ using Lab.Entities;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
+    public class CombineKeyCompare : IComparer<Employee>
+    {
+        public CombineKeyCompare(Func<Employee, string> keySelector, IComparer<string> keyComparer)
+        {
+            KeySelector = keySelector;
+            KeyComparer = keyComparer;
+        }
+
+        public Func<Employee, string> KeySelector { get; private set; }
+        public IComparer<string> KeyComparer { get; private set; }
+
+        public int Compare(Employee element, Employee minElement)
+        {
+            return this.KeyComparer.Compare(this.KeySelector(element), this.KeySelector(minElement));
+        }
+    }
+
     [TestFixture]
     public class JoeyOrderByTests
     {
-        //[Test]
-        //public void orderBy_lastName()
-        //{
-        //    var employees = new[]
-        //    {
-        //        new Employee {FirstName = "Joey", LastName = "Wang"},
-        //        new Employee {FirstName = "Tom", LastName = "Li"},
-        //        new Employee {FirstName = "Joseph", LastName = "Chen"},
-        //        new Employee {FirstName = "Joey", LastName = "Chen"},
-        //    };
-
-        //    var actual = JoeyOrderByLastName(employees);
-
-        //    var expected = new[]
-        //    {
-        //        new Employee {FirstName = "Joseph", LastName = "Chen"},
-        //        new Employee {FirstName = "Joey", LastName = "Chen"},
-        //        new Employee {FirstName = "Tom", LastName = "Li"},
-        //        new Employee {FirstName = "Joey", LastName = "Wang"},
-        //    };
-
-        //    expected.ToExpectedObject().ShouldMatch(actual);
-        //}
-
         [Test]
         public void orderBy_lastName_and_firstName()
         {
@@ -46,7 +38,9 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Joey", LastName = "Chen"},
             };
 
-            var actual = JoeyOrderByLastName(employees);
+            var firstCompare = new CombineKeyCompare(element => element.LastName, Comparer<string>.Default);
+            var lastCompare = new CombineKeyCompare(element => element.FirstName, Comparer<string>.Default);
+            var actual = JoeyOrderByLastNameAndFirstName(employees, firstCompare, lastCompare);
 
             var expected = new[]
             {
@@ -59,10 +53,9 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<Employee> JoeyOrderByLastName(IEnumerable<Employee> employees)
+        private IEnumerable<Employee> JoeyOrderByLastNameAndFirstName(IEnumerable<Employee> employees, IComparer<Employee> firstCompare, IComparer<Employee> secondCompare)
         {
             //bubble sort
-            var stringComparer = StringComparer.Create(CultureInfo.CurrentCulture, true);
             var elements = employees.ToList();
             while (elements.Any())
             {
@@ -70,10 +63,22 @@ namespace CSharpAdvanceDesignTests
                 var index = 0;
                 for (int i = 1; i < elements.Count; i++)
                 {
-                    if (stringComparer.Compare(elements[i].LastName + elements[i].FirstName, minElement.LastName + minElement.FirstName) < 0)
+                    var element = elements[i];
+
+                    var firstCompareResult = firstCompare.Compare(element, minElement);
+                    if (firstCompareResult < 0)
                     {
-                        minElement = elements[i];
+                        minElement = element;
                         index = i;
+                    }
+                    else if (firstCompareResult == 0)
+                    {
+                        var secondCompareResult = secondCompare.Compare(element, minElement);
+                        if (secondCompareResult < 0)
+                        {
+                            minElement = element;
+                            index = i;
+                        }
                     }
                 }
 
