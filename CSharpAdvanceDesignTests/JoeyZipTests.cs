955ec8ee81535967c1e965a4211c0eae14cd78dd
Owen <owen2862@live.com>
2019-03-16 11:00:58 +0800

    zip

--- CSharpAdvanceDesignTests/JoeyZipTests.cs
+++ CSharpAdvanceDesignTests/JoeyZipTests.cs
@@ -2,6 +2,7 @@
 using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using System;
 using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
@@ -25,7 +26,7 @@ namespace CSharpAdvanceDesignTests
                 new Key() {Type = CardType.Benz, Owner = "Tom"},
             };
 
-            var pairs = JoeyZip(girls, keys);
+            var pairs = JoeyZip(girls, keys, (girl, key) => $"{girl.Name}-{key.Owner}");
 
             var expected = new[]
             {
@@ -36,14 +37,43 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(pairs);
         }
 
-        private IEnumerable<string> JoeyZip(IEnumerable<Girl> girls, IEnumerable<Key> keys)
+        [Test]
+        public void pair_girls_and_keys_car_type()
         {
-            var girlEnumerator = girls.GetEnumerator();
-            var keyEnumerator = keys.GetEnumerator();
-
-            while (girlEnumerator.MoveNext() && keyEnumerator.MoveNext())
+            var girls = new List<Girl>
             {
-                yield return $"{girlEnumerator.Current.Name}-{keyEnumerator.Current.Owner}";
+                new Girl() {Name = "Mary"},
+                new Girl() {Name = "Jessica"},
+            };
+
+            var keys = new List<Key>
+            {
+                new Key() {Type = CardType.BMW, Owner = "Joey"},
+                new Key() {Type = CardType.TOYOTA, Owner = "David"},
+                new Key() {Type = CardType.Benz, Owner = "Tom"},
+            };
+
+            var pairs = JoeyZip(girls, keys, (girl, key) => $"{girl.Name}-{key.Type.ToString()}");
+
+            var expected = new[]
+            {
+                "Mary-BMW",
+                "Jessica-TOYOTA",
+            };
+
+            expected.ToExpectedObject().ShouldMatch(pairs);
+        }
+
+        private IEnumerable<TResult> JoeyZip<TSource1, TSource2, TResult>(IEnumerable<TSource1> source1, IEnumerable<TSource2> source2, Func<TSource1, TSource2, TResult> func)
+        {
+            var source1Enumerator = source1.GetEnumerator();
+            var source2Enumerator = source2.GetEnumerator();
+
+            while (source1Enumerator.MoveNext() && source2Enumerator.MoveNext())
+            {
+                var result1 = source1Enumerator.Current;
+                var result2 = source2Enumerator.Current;
+                yield return func(result1, result2);
             }
         }
     }
