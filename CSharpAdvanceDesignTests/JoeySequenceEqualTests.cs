df09961b1fc2b9b8527caeb8da82eefa08ae8b40
Owen <owen2862@live.com>
2019-03-16 12:43:23 +0800

    sequenceEqual

--- CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
+++ CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
@@ -1,11 +1,9 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeySequenceEqualTests
     {
         [Test]
@@ -19,9 +17,75 @@ namespace CSharpAdvanceDesignTests
             Assert.IsTrue(actual);
         }
 
+        [Test]
+        public void compare_two_numbers_not_equal()
+        {
+            var first = new List<int> { 3, 2, 1 };
+            var second = new List<int> { 1, 2, 3 };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_not_equal_length1()
+        {
+            var first = new List<int> { 3, 2 };
+            var second = new List<int> { 3, 2, 1 };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_not_equal_length2()
+        {
+            var first = new List<int> { 3, 2, 1 };
+            var second = new List<int> { 3, 2 };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_empty()
+        {
+            var first = new List<int> { };
+            var second = new List<int> { };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsTrue(actual);
+        }
+
         private bool JoeySequenceEqual(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new NotImplementedException();
+            var firstEnumerator = first.GetEnumerator();
+            var secondEnumerator = second.GetEnumerator();
+
+            while (true)
+            {
+                var tempFirst = firstEnumerator.MoveNext();
+                var tempSecond = secondEnumerator.MoveNext();
+                //長度不同
+                if (tempFirst != tempSecond)
+                {
+                    return false;
+                }
+                //資料不同
+                if (firstEnumerator.Current != secondEnumerator.Current)
+                {
+                    return false;
+                }
+                //因為前面已經判斷過長度與資料了，因此若此時first是false,表示second也是false,而且到目前為止資料都相同。故可判斷此時集合已經判斷結束
+                if (!tempFirst)
+                {
+                    return true;
+                }
+            }
         }
     }
 }
