d762a4756f7c2eb436cf590ef6b829ea469ad633
Owen <owen2862@live.com>
2019-03-17 10:23:45 +0800

    UNION

--- CSharpAdvanceDesignTests/JoeyUnionTests.cs
+++ CSharpAdvanceDesignTests/JoeyUnionTests.cs
@@ -1,18 +1,18 @@
 ﻿using ExpectedObjects;
+using Lab.Entities;
 using NUnit.Framework;
 using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyUnionTests
     {
         [Test]
         public void union_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 3, 7 };
+            var first = new[] { 1, 3, 5, 3, 1 };
+            var second = new[] { 5, 3, 7, 7 };
 
             var actual = JoeyUnion(first, second);
             var expected = new[] { 1, 3, 5, 7 };
@@ -20,9 +20,48 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
+        private IEnumerable<Employee> JoeyUnion(IEnumerable<Employee> first, IEnumerable<Employee> second, EmployeeCompare comparer)
+        {
+            var hash = new HashSet<Employee>(comparer);
+            var enumerator = first.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (hash.Add(enumerator.Current))
+                {
+                    yield return enumerator.Current;
+                }
+            }
+
+            var secondEnumerator = second.GetEnumerator();
+            while (secondEnumerator.MoveNext())
+            {
+                if (hash.Add(secondEnumerator.Current))
+                {
+                    yield return secondEnumerator.Current;
+                }
+            }
+        }
+
         private IEnumerable<int> JoeyUnion(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var hash = new HashSet<int>();
+            var enumerator = first.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (hash.Add(enumerator.Current))
+                {
+                    yield return enumerator.Current;
+                }
+            }
+
+            var secondEnumerator = second.GetEnumerator();
+            while (secondEnumerator.MoveNext())
+            {
+                if (hash.Add(secondEnumerator.Current))
+                {
+                    yield return secondEnumerator.Current;
+                }
+            }
         }
     }
 }
